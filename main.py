fd61e02a4aefacbbd5043007fd43a9a823ab60f2
Oved Haruvi <t-ovharu@microsoft.com>
2021-01-09 17:54:13 +0200

    Fixed after debugging 1

--- main.py
+++ main.py
@@ -2,15 +2,27 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from LF3 import *
+from LFutils import *
+import numpy as np
+import cv2
+import matplotlib.pylab as plt
+from skimage import transform, io , color
 
+img = io.imread('neon_7.gif')
+img = img[:, :, :-1]
+print('Image shape:{}, Image data type:{}'.format(img.shape, img.dtype))
+plt.imshow(img, cmap='gray')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X & Y axis
+plt.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+filled_img = line_filling(img)
+plt.imshow(filled_img, cmap='gray')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X & Y axis
+plt.show()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+img = color.rgb2gray(img)
+plt.imshow(img - filled_img, cmap='gray')
+plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
